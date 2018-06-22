7a809657c2212c034142d12a95aa377ce0ae0bd4
Aswin Vijayakumar <aswinvk28@gmail.com>
2018-06-22 21:09:13 +0100

    Submission

--- diffusion.cc
+++ diffusion.cc
@@ -34,9 +34,11 @@ int diffusion(const int n_particles,
               VSLStreamStatePtr rnStream) {
   
   int n_escaped=0;
-  int number_split = 1<<5;
+  int pow = 2;
+  int shift_register = 5;
+  int number_split = 1<<shift_register;
   int w = 0, s = 0;
-  float p[n_steps][number_split];
+  float rn[number_split];
   int p_particles = n_particles;
   float pos[n_particles];
   int idx;
@@ -45,28 +47,28 @@ int diffusion(const int n_particles,
     pos[i] = 0.0f;
   }
 
-  while(true) {
+  
+  while(s < n_steps) {
     idx = 0;
-    if(partition_func(&p_particles, n_steps, number_split) == true) {
-      while(w < n_steps) {
-        int errorcode = vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, number_split, p[w], -1.0f, 1.0f);
-        if(errorcode != VSL_STATUS_OK) {
-          printf("Error code %d", errorcode);
-        }
-        w += 1;
+    if(partition_func(&p_particles, n_steps, number_split^pow) == true) {
+      int errorcode = vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, number_split^pow, rn, -1.0f, 1.0f);
+      if(errorcode != VSL_STATUS_OK) {
+        printf("Error code %d", errorcode);
       }
-      #pragma omp parallel for
-      for (int j = 0; j < n_steps; j++) {
-        //Intel MKL function to generate random numbers
-        #pragma omp simd
-        for(int k = 0; k < number_split; k++) {
-          pos[k + idx] = dist_func(pos[idx], alpha, p[j][k]);
+      int p_idx = 0;
+      //Intel MKL function to generate random numbers
+      for(int k = 0; k < number_split^(pow/2); k++) {
+        for(int i = 0; i < number_split^(pow/2); i++) {
+          pos[k + idx] = dist_func(pos[idx], alpha, rn[p_idx + idx]);
+          #pragma omp atomic
+          p_idx += 1;
         }
       }
-      idx += number_split;
+      idx += number_split^pow;
     } else {
       break;
     }
+    s += 1;
   }
 
   #pragma omp simd reduction(+: n_escaped)
